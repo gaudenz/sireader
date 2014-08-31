08eea6ed2f0932518f2c4bdcd18b574c1eb40dc9
Gaudenz Steinlin <gaudenz@soziologie.ch>
2014-08-31 23:58:35 +0200

    Python packageing
    Add required files to make this a Python package. The README is empty at
    the moment and will be filled later.

---
+++ setup.py
@@ -0,0 +1,63 @@
+from setuptools import setup
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='sireader',
+
+    version='1.0.0.dev',
+
+    description='Python module to communicate with SportIdent stations',
+    long_description=long_description,
+
+    url='http://bosco.durcheinandertal.ch/',
+
+    # Author details
+    author='Gaudenz Steinlin',
+    author_email='gaudenz@durcheinandertal.ch',
+
+    # Choose your license
+    license='GPLv3+',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: System :: Hardware :: Hardware Drivers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    # What does your project relate to?
+    keywords='sportident orienteering bosco',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    py_modules=['sireader'],
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
+    install_requires=['pyserial'],
+)
