760144f10e54368f18bf795f49236df17dd1df94
Gaudenz Steinlin <gaudenz@durcheinandertal.ch>
2014-11-05 11:42:12 +0100

    Add six dependency
    This is needed for correct bytes handling in Python 2.x

--- setup.py
+++ setup.py
@@ -59,5 +59,5 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=['pyserial'],
+    install_requires=['pyserial', 'six'],
 )
